310960991f8e0f0ba68922d2584c995b496c1736
Taylor <115taegyeong@gmail.com>
2024-10-16 20:03:07 +0900

    feat: section6 service layer test

---
+++ src/test/java/sample/cafekiosk/spring/domain/order/OrderTest.java
@@ -0,0 +1,55 @@
+package sample.cafekiosk.spring.domain.order;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import sample.cafekiosk.spring.domain.product.Product;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static sample.cafekiosk.spring.domain.order.OrderStatus.*;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
+import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
+
+class OrderTest {
+
+    @Test
+    @DisplayName("주문 생성 시 상품 리스트에서 주문의 총 금액을 계산한다.")
+    void calculateTotalPrice() {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        List<Product> products = List.of(
+                createProduct("001", 1000),
+                createProduct("002", 2000)
+        );
+
+        // when
+        Order order = Order.create(products, now);
+
+        //then
+        assertThat(order.getTotalPrice()).isEqualTo(3000);
+    }
+
+    @Test
+    @DisplayName("주문 생성 시 주문 상태는 INIT이다.")
+    void init() {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        List<Product> products = List.of(
+                createProduct("001", 1000),
+                createProduct("002", 2000)
+        );
+
+        // when
+        Order order = Order.create(products, now);
+
+        //then
+        assertThat(order.getOrderStatus()).isEqualTo(INIT);
+    }
+
+    private Product createProduct(String productNumber, int price) {
+        return Product.builder().type(HANDMADE).productNumber(productNumber).price(price).sellingStatus(SELLING).name("메뉴 이름").build();
+    }
+
+}
