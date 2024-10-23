aad122d95a90220acc5649383ae410038b370913
Taylor <115taegyeong@gmail.com>
2024-10-23 19:43:42 +0900

    feat: section9 spring rest doc

--- src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class ProductContorller {
+public class ProductController {
 
     private final ProductService productService;
 
